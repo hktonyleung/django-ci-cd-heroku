8d45fa9d95599bef6b597c93380e5f3cf7193fb1
hktonyleung <hktonyleung@gmail.com>
2021-10-22 07:05:49 +0800

    amend allowed host

--- django_ci_cd_heroku/settings.py
+++ django_ci_cd_heroku/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://django-ci-cd-testing.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["django-ci-cd-testing.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
